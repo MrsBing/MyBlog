0a4da683dc4ce711b74c09e3f76e73f525179c08
MrsBing <1481084297@qq.com>
2017-08-16 21:24:08 +0800

    创建项目初始结构

---
+++ application/views/index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>首页</title>
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <base href="<?php echo site_url();?>">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/index.css">
+    <script>
+        window.addEventListener("load", function () {
+            setTimeout(hideURLbar, 0);
+        }, false);
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
+    </script>
+</head>
+<body>
+<script src="js/jquery-1.11.1.min.js"></script>
+<script>
+	$(function (){
+
+	});
+</script>
+</body>
+</html>
